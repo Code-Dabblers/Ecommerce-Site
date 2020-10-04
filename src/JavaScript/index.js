2ef556302011a25c373d0312d78bf4fccd45efdb
ZachyDev <mosetizachary001@gmail.com>
2020-10-04 16:04:51 +0300

    Added a copyright and year to the footer✅

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -1,1 +1,12 @@
-console.log("it works")
+// get the span for copyright
+const copyrightSpan = document.getElementById('copyright');
+
+// define the date object 
+const date = new Date();
+
+// get the year
+const year = date.getFullYear();
+
+// update the copyright div
+
+copyrightSpan.innerHTML = `Ecomm &copy; ${year} All Rights Reserved.`;

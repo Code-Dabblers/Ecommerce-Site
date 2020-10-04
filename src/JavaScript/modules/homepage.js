72081374030b1056f7af9cc3b0b16b1f713bcf0d
fabcodingzest <fabrizvi786@gmail.com>
2020-10-05 00:51:42 +0530

    navigo and lit-html set up for working

---
+++ src/JavaScript/modules/homepage.js
@@ -0,0 +1,7 @@
+import { html } from "../../../../node_modules/lit-html/lit-html.js";
+
+const homepage = () => {
+  return html`<h1 class="random">Its home page</h1>`;
+};
+
+export default homepage;
